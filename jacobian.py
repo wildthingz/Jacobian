2e83fb73c72af086274f3e6cb7cf3c7cc27f43bb
Hatef Khadivinassab <hatef.khadivinassab@gmail.com>
2018-11-16 19:17:00 -0800

    changes stuff

--- jacobian.py
+++ jacobian.py
@@ -51,15 +51,15 @@ class ARM_FTM:
         self.L2 = L2
         self.L3 = L3
 
-        self.SF_ROT     = lambda θ : np.array([[1, 0, 0], [0, np.cos(θ), np.sin(θ)], [0, -np.sin(θ), np.cos(θ)]])
-        self.SA_ROT     = lambda θ : np.array([[np.cos(θ), 0, np.sin(θ)], [0, 1, 0], [-np.sin(θ), 0, np.cos(θ)]])
-        self.SR_ROT     = lambda θ : np.array([[np.cos(θ), -np.sin(θ), 0], [np.sin(θ), np.cos(θ), 0], [0, 0, 1]])
-        self.SR_TRANS   = np.array([[0], [0], [self.L1]])
-        self.EF_ROT     = lambda θ : np.array([[1, 0, 0], [0, np.cos(θ), np.sin(θ)], [0, -np.sin(θ), np.cos(θ)]])
-        self.ER_ROT     = lambda θ : np.array([[np.cos(θ), -np.sin(θ), 0], [np.sin(θ), np.cos(θ), 0], [0, 0, 1]])
-        self.ER_TRANS   = np.array([[0], [0], [self.L2]])
-        self.WA_ROT     = lambda θ : np.array([[np.cos(θ), 0, np.sin(θ)], [0, 1, 0], [-np.sin(θ), 0, np.cos(θ)]])
-        self.WA_TRANS   = np.array([[0], [0], [self.L3]])
+        self.SF_ROT     = lambda θ : np.array([[1, 0, 0, 0], [0, np.cos(θ), np.sin(θ), 0], [0, -np.sin(θ), np.cos(θ), 0], [0, 0, self.L1, 1]])
+        self.SA_ROT     = lambda θ : np.array([[np.cos(θ), 0, np.sin(θ), 0], [0, 1, 0, 0], [-np.sin(θ), 0, np.cos(θ), 0], [0, 0, self.L1, 1]])
+        self.SR_ROT     = lambda θ : np.array([[np.cos(θ), -np.sin(θ), 0, 0], [np.sin(θ), np.cos(θ), 0, 0], [0, 0, 1, 0], [0, 0, self.L1, 1]])
+        self.SR_TRANS   = np.array([[0], [0], [self.L1], [1]])
+        self.EF_ROT     = lambda θ : np.array([[1, 0, 0, 0], [0, np.cos(θ), np.sin(θ), 0], [0, -np.sin(θ), np.cos(θ), 0], [0, 0, 0, 1]])
+        self.ER_ROT     = lambda θ : np.array([[np.cos(θ), -np.sin(θ), 0, 0], [np.sin(θ), np.cos(θ), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        self.ER_TRANS   = np.array([[0], [0], [self.L2], [1]])
+        self.WA_ROT     = lambda θ : np.array([[np.cos(θ), 0, np.sin(θ), 0], [0, 1, 0, 0], [-np.sin(θ), 0, np.cos(θ), 0], [0, 0, 0, 1]])
+        self.WA_TRANS   = np.array([[0], [0], [self.L3], [1]])
 
 
 def main():
@@ -70,8 +70,8 @@ def main():
     point = np.array([[0, 0, 0, 1]])
 
     theta_SF_ROT = 40 * np.pi / 180
-    theta_SA_ROT = 30 * np.pi / 180
-    theta_SR_ROT = 20 * np.pi / 180
+    theta_SA_ROT = 20 * np.pi / 180
+    theta_SR_ROT = 10 * np.pi / 180
     theta_EF_ROT = 0 * np.pi / 180
     theta_ER_ROT = 0 * np.pi / 180
     theta_WA_ROT = 0 * np.pi / 180
@@ -90,28 +90,32 @@ def main():
 
     #shoulder joint
     transform_SF2SA = np.matmul(transform_SF_ROT, transform_SA_ROT)
+    print(transform_SF2SA)
+    print("\n")
     transform_SA2SR = np.matmul(transform_SF2SA, transform_SR_ROT)
-    print(transform_SA2SR)
-    transform_S_TRANS = np.concatenate((transform_SA2SR, transform_SR_TRANS), axis = 1)
-    print(transform_S_TRANS)
-    transform_S = np.concatenate((transform_S_TRANS, np.array([[0, 0, 0, 1]])))
-    print(transform_S)
+#    print(transform_SA2SR)
+#    print("\n")
+#    transform_S_TRANS = transform_SA2SR[:,]transform_SR_TRANS), axis = 1)
+#    print(transform_S_TRANS)
+#    print("\n")
+#    transform_S = np.concatenate((transform_S_TRANS, np.array([[0, 0, 0, 1]])))
+#    print(transform_S)
 
     #elbow joint
-    transform_EF2ER = np.matmul(transform_EF_ROT, transform_ER_ROT)
-    transform_E_TRANS = np.concatenate((transform_EF2ER, transform_ER_TRANS), axis = 1)
-    transform_E = np.concatenate((transform_E_TRANS, np.array([[0, 0, 0, 1]])))
+#    transform_EF2ER = np.matmul(transform_EF_ROT, transform_ER_ROT)
+#    transform_E_TRANS = np.concatenate((transform_EF2ER, transform_ER_TRANS), axis = 1)
+#    transform_E = np.concatenate((transform_E_TRANS, np.array([[0, 0, 0, 1]])))
 
     #wrist joint
-    transform_W_TRANS = np.concatenate((transform_WA_ROT, transform_WA_TRANS), axis = 1)
-    transform_W = np.concatenate((transform_W_TRANS, np.array([[0, 0, 0, 1]])))
+#    transform_W_TRANS = np.concatenate((transform_WA_ROT, transform_WA_TRANS), axis = 1)
+#    transform_W = np.concatenate((transform_W_TRANS, np.array([[0, 0, 0, 1]])))
 
     #combination
-    transform_S2E = np.matmul(transform_S, transform_E)
-    end_transform = np.matmul(transform_S2E, transform_W)
+#    transform_S2E = np.matmul(transform_S, transform_E)
+#    end_transform = np.matmul(transform_S2E, transform_W)
 
     #print(end_transform)
-    endpoint = np.matmul(transform_S, point.T)
+    endpoint = np.matmul(transform_SA2SR, point.T)
 
     print(endpoint)
 
